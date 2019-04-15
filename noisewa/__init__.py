db6aafd916611a62d5e9a3634ee8d3ac605d015a
Tarek Galal <tare2.galal@gmail.com>
2019-04-15 08:38:29 +0200

    [log] configure top level logger

---
+++ noisewa/__init__.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(levelname).1s %(asctime)s %(name)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
