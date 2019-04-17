078b6caddab36f1dfaf9c3bcd72028a05439fd9b
Tarek Galal <tare2.galal@gmail.com>
2019-04-17 15:45:41 +0200

    [doc] explain in WASymmetricState why it exists

--- consonance/dissononce/processing/symmetricstate_wa.py
+++ consonance/dissononce/processing/symmetricstate_wa.py
@@ -2,6 +2,8 @@ from dissononce.processing.impl.symmetricstate import SymmetricState
 
 
 class WASymmetricState(SymmetricState):
+    """WA does not mix_hash if cipherstate does not yet have a key, therefore this custom WASymmetricState has to
+    be used instead of default one."""
     def encrypt_and_hash(self, plaintext):
         ciphertext = self._cipherstate.encrypt_with_ad(self._h, plaintext)
         if self._cipherstate.has_key():
