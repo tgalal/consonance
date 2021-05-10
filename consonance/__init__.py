390945393df57efc92a9453f4b3bf0096aa77bc2
GM12Tick <>
2021-05-10 08:47:01 +0300

    Updating Consonance to 4 parts app version

--- consonance/__init__.py
+++ consonance/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Tarek Galal"
 
 logger = logging.getLogger(__name__)
--- consonance/config/appversion.py
+++ consonance/config/appversion.py
@@ -2,7 +2,8 @@ class AppVersionConfig(object):
     STR_TEMPLATE = """AppVersionConfig(
             primary={primary},
             secondary={secondary},
-            tertiary={tertiary}
+            tertiary={tertiary},
+            quaternary={quaternary}
         )"""
 
     def __init__(self, version):
@@ -11,14 +12,15 @@ class AppVersionConfig(object):
         :type version: str
         """
         dissected = version.split('.')
-        assert len(dissected) > 2, "version must be in format x.y.z"
-        self._primary, self._secondary, self._tertiary = map(lambda v:int(v), dissected)
+        assert len(dissected) > 3, "version must be in format x.y.z"
+        self._primary, self._secondary, self._tertiary, self._quaternary = map(lambda v:int(v), dissected)
 
     def __str__(self):
         return self.STR_TEMPLATE.format(
             primary=self.primary,
             secondary=self.secondary,
-            tertiary=self.tertiary
+            tertiary=self.tertiary,
+            quaternary=self.quaternary
         )
 
     @property
@@ -32,3 +34,7 @@ class AppVersionConfig(object):
     @property
     def tertiary(self):
         return self._tertiary
+
+    @property
+    def quaternary(self):
+        return self._quaternary
